db5daf0de510170dccd02b123dfdc3c04dff07c8
o-n-e <stewart.barron@mac.com>
2016-05-10 16:40:31 +0100

    initial commit

---
+++ spec/queryBuilder.spec.js
@@ -0,0 +1,103 @@
+describe('queryBuilder', function() {
+
+    var queryData = {};
+    var table = "MAIN_TABLE"
+
+    beforeEach(function() {
+        queryData = {
+            "name": "queryData",
+            "value": [
+                {
+                    "name": "outputs",
+                    "value": [
+                        {
+                            "name": "I1",
+                            "type": "group"
+                        },
+                        {
+                            "name": "F1",
+                            "type": "group"
+                        },
+                        {
+                            "name": "D1",
+                            "type": "aggregate",
+                            "aggType": "SUM"
+                        },
+                        {
+                            "name": "D2",
+                            "type": "aggregate",
+                            "aggType": "COUNT"
+                        }
+                    ]
+                },
+                {
+                    "name": "filter",
+                    "value": [
+                        {
+                            "name": "start_date",
+                            "value": [
+                                '30-04-2015',
+                                '31-04-2015'
+                            ]
+                        },
+                        {
+                            "name": "i_code",
+                            "value": [10,20]
+                        }
+                    ]
+                }
+            ]
+        };
+    });
+
+    it('should build a select statement', function() {
+        var builder = queryBuilder(table, queryData);
+        var sql = builder.buildSelect()
+            .print();
+        var expected = "SELECT I1,F1,SUM(D1),COUNT(D2) ";
+        expect(sql).toBe(expected);
+    });
+
+    it('should build a select from statement', function() {
+        var builder = queryBuilder(table, queryData);
+        var sql = builder.buildSelect()
+            .buildFrom()
+            .print();
+        var expected = "SELECT I1,F1,SUM(D1),COUNT(D2) FROM MAIN_TABLE ";
+        expect(sql).toBe(expected);
+    });
+
+    it('should build a select from where statement', function() {
+        var builder = queryBuilder(table, queryData);
+        var sql = builder.buildSelect()
+            .buildFrom()
+            .buildWhere()
+            .print();
+        var expected = "SELECT I1,F1,SUM(D1),COUNT(D2) FROM MAIN_TABLE WHERE start_date IN ('30-04-2015','31-04-2015') AND i_code IN (10,20) ";
+        expect(sql).toBe(expected);
+    });
+
+    it('should build a select from where group by statement', function() {
+        var builder = queryBuilder(table, queryData);
+        var sql = builder.buildSelect()
+            .buildFrom()
+            .buildWhere()
+            .buildGroupBy()
+            .print();
+        var expected = "SELECT I1,F1,SUM(D1),COUNT(D2) FROM MAIN_TABLE WHERE start_date IN ('30-04-2015','31-04-2015') AND i_code IN (10,20) GROUP BY I1,F1 ";
+        expect(sql).toBe(expected);
+    });
+
+    it('should build a select from where group by order by statement', function() {
+        var builder = queryBuilder(table, queryData);
+        var sql = builder.buildSelect()
+            .buildFrom()
+            .buildWhere()
+            .buildGroupBy()
+            .buildOrderBy()
+            .print();
+        var expected = "SELECT I1,F1,SUM(D1),COUNT(D2) FROM MAIN_TABLE WHERE start_date IN ('30-04-2015','31-04-2015') AND i_code IN (10,20) GROUP BY I1,F1 ORDER BY I1,F1 ";
+        expect(sql).toBe(expected);
+    });
+
+});
---
+++ src/queryBuilder.js
@@ -0,0 +1,113 @@
+var queryBuilder = function(table, queryParams) {
+
+    var queryParams = queryParams;
+
+    var sql = '';
+
+    var buildSelect = function() {
+        var cols = queryParams.value[0];
+        var col = {};
+        sql = "SELECT ";
+
+        for(var i = 0; i < cols.value.length; i++) {
+
+            col = cols.value[i];
+
+            if (col["type"] === "group") {
+                sql += col["name"] + ",";
+            } else if (col["type"] === "aggregate") {
+                sql += col["aggType"] + "(" + col["name"] + "),";
+            }
+        }
+
+        sql = sql.slice(0, -1);
+        sql += ' ';
+
+        return this;
+    };
+
+    var buildFrom = function() {
+        sql += "FROM " + table + " ";
+
+        return this;
+    };
+
+    var buildWhere= function() {
+        var conditions = queryParams.value[1];
+        sql += "WHERE ";
+
+        for(var i = 0; i < conditions.value.length; i++) {
+
+            var thisName = conditions.value[i].name;
+            var thisValue = conditions.value[i].value;
+
+            sql += thisName + " IN (";
+
+            for (j = 0; j < thisValue.length; j++) {
+                if (typeof thisValue[j] === 'number') {
+
+                    sql += thisValue[j] + ',';
+                }
+                else {
+                    sql = sql  + "'" + thisValue[j] + "',";
+                }
+            }
+            sql = sql.slice(0, -1);
+            sql += ") AND ";
+        }
+
+        sql = sql.slice(0, -4);
+
+        return this;
+    };
+
+    var buildGroupBy = function() {
+        var cols = queryParams.value[0];
+        var col = {};
+        sql += "GROUP BY ";
+
+        for(var i = 0; i < cols.value.length; i++) {
+            col = cols.value[i];
+            if (col["type"] === "group") {
+                sql += col["name"] + ",";
+            }
+        }
+
+        sql = sql.slice(0, -1);
+        sql += ' ';
+
+        return this;
+    };
+
+    var buildOrderBy = function() {
+        var cols = queryParams.value[0];
+        var col = {};
+        sql += "ORDER BY ";
+
+        for(var i = 0; i < cols.value.length; i++) {
+            col = cols.value[i];
+            if (col["type"] === "group") {
+                sql += col["name"] + ",";
+            }
+        }
+
+        sql = sql.slice(0, -1);
+        sql += ' ';
+
+        return this;
+    };
+
+    var print = function() {
+        return sql;
+    }
+
+    return {
+            buildSelect:buildSelect,
+            buildFrom:buildFrom,
+            buildWhere:buildWhere,
+            buildGroupBy:buildGroupBy,
+            buildOrderBy:buildOrderBy,
+            print:print
+    };
+
+};
